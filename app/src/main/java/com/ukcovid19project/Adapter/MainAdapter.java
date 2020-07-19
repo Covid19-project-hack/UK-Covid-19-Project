e587eef69eef7208a0eecfa24989c25eeaacdf0b
Anandha Murthy B <anandhamurthy2000@gmail.com>
2020-07-19 16:21:51 +0530

    Initial commit

---
+++ app/src/main/java/com/ukcovid19project/Adapter/MainAdapter.java
@@ -0,0 +1,201 @@
+package com.ukcovid19project.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.ukcovid19project.HealthCareActivity;
+import com.ukcovid19project.MedicalStoresActivity;
+import com.ukcovid19project.OrphanageSupport.FreeFoodActivity;
+import com.bumptech.glide.Glide;
+
+import com.ukcovid19project.CoronaActivity;
+import com.ukcovid19project.FAQsActivity;
+import com.ukcovid19project.HomeTreamentActivity;
+import com.ukcovid19project.HospitalAdmissionActivity;
+import com.ukcovid19project.Models.Jsons;
+import com.ukcovid19project.MyHealthStatusActivity;
+import com.ukcovid19project.OnlineDoctorsActivity;
+import com.ukcovid19project.OnlineEducationActivity;
+import com.ukcovid19project.OrphanageSupportActivity;
+import com.ukcovid19project.R;
+import com.ukcovid19project.TestLabsActivity;
+import com.ukcovid19project.TweetsActivity;
+import com.ukcovid19project.VolunteersActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private int[] Image;
+    private String[] Title;
+    private DatabaseReference mJsonDatabase;
+
+    public MainAdapter(Context context, String[] titles, int[] images) {
+        mContext = context;
+        Image = images;
+        Title = titles;
+        mJsonDatabase = FirebaseDatabase.getInstance().getReference().child("Jsons");
+        mJsonDatabase.keepSynced(true);
+    }
+
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.single_grid_item, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ImageViewHolder holder, final int position) {
+
+        holder.Title.setText(Title[position]);
+        Glide.with(mContext)
+                .load(Image[position])
+                .into(holder.Image);
+        mJsonDatabase = FirebaseDatabase.getInstance().getReference().child("Jsons");
+        mJsonDatabase.keepSynced(true);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mJsonDatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final Jsons jsons = dataSnapshot.getValue(Jsons.class);
+                        switch (holder.getAdapterPosition()) {
+                            case 0:
+                                Intent coronaintent = new Intent(mContext, CoronaActivity.class);
+                                coronaintent.putExtra("url", jsons.getCorona());
+                                mContext.startActivity(coronaintent);
+                                break;
+                            case 1:
+                                Intent hometreatmentintent = new Intent(mContext, HomeTreamentActivity.class);
+                                hometreatmentintent.putExtra("image_url", jsons.getHome_treatment_images());
+                                hometreatmentintent.putExtra("link_url", jsons.getHome_treatment_links());
+                                mContext.startActivity(hometreatmentintent);
+                                break;
+                            case 2:
+                                Intent myhealthstatusintent = new Intent(mContext, MyHealthStatusActivity.class);
+                                mContext.startActivity(myhealthstatusintent);
+                                break;
+
+                            case 3:
+                                Intent healthcareintent = new Intent(mContext, HealthCareActivity.class);
+                                mContext.startActivity(healthcareintent);
+                                break;
+
+                            case 4:
+                                Intent medicalstoresintent = new Intent(mContext, MedicalStoresActivity.class);
+                                mContext.startActivity(medicalstoresintent);
+                                break;
+                            case 5:
+                                Intent onlinedoctorintent = new Intent(mContext, OnlineDoctorsActivity.class);
+                                mContext.startActivity(onlinedoctorintent);
+                                break;
+                            case 6:
+                                Intent hospitaladmissionintent = new Intent(mContext, HospitalAdmissionActivity.class);
+                                mContext.startActivity(hospitaladmissionintent);
+                                break;
+
+                            case 7:
+                                Intent volunteersintent = new Intent(mContext, VolunteersActivity.class);
+                                mContext.startActivity(volunteersintent);
+                                break;
+                            case 8:
+                                Intent freefoodintent = new Intent(mContext, FreeFoodActivity.class);
+                                mContext.startActivity(freefoodintent);
+                                break;
+
+                            case 9:
+                                Intent testlabsintent = new Intent(mContext, TestLabsActivity.class);
+                                testlabsintent.putExtra("url", jsons.getLab_test());
+                                mContext.startActivity(testlabsintent);
+                                break;
+
+                            case 10:
+                                Intent orphanagesupportintent = new Intent(mContext, OrphanageSupportActivity.class);
+                                mContext.startActivity(orphanagesupportintent);
+                                break;
+
+                            case 11:
+                                Intent epassintent = new Intent(Intent.ACTION_VIEW);
+                                epassintent.setData(Uri.parse(jsons.getEpass()));
+                                mContext.startActivity(epassintent);
+                                break;
+
+                            case 12:
+                                Intent donateintent = new Intent(Intent.ACTION_VIEW);
+                                donateintent.setData(Uri.parse(jsons.getDonate()));
+                                mContext.startActivity(donateintent);
+                                break;
+
+                            case 13:
+                                Intent educationintent = new Intent(mContext, OnlineEducationActivity.class);
+                                educationintent.putExtra("cbse",jsons.getCbse());
+                                educationintent.putExtra("vc",jsons.getVocational_education());
+                                mContext.startActivity(educationintent);
+                                break;
+
+                            case 14:
+                                Intent intent20 = new Intent(Intent.ACTION_VIEW);
+                                intent20.setData(Uri.parse(jsons.getGo()));
+                                mContext.startActivity(intent20);
+                                break;
+
+                            case 15:
+                                Intent tweetintent = new Intent(mContext, TweetsActivity.class);
+                                tweetintent.putExtra("url", jsons.getTweets());
+                                mContext.startActivity(tweetintent);
+                                break;
+                            case 16:
+                                Intent faqintent = new Intent(mContext, FAQsActivity.class);
+                                faqintent.putExtra("url", jsons.getFaq());
+                                mContext.startActivity(faqintent);
+                                break;
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Title.length;
+    }
+
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView Title;
+        public ImageView Image;
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            Title = itemView.findViewById(R.id.label);
+            Image = itemView.findViewById(R.id.image);
+        }
+    }
+}
---
+++ app/src/main/java/com/ukcovid19project/MainActivity.java
@@ -0,0 +1,247 @@
+package com.ukcovid19project;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.ukcovid19project.Adapter.GridAdapter;
+import com.ukcovid19project.Adapter.MainAdapter;
+import com.ukcovid19project.Login.LoginActivity;
+import com.ukcovid19project.Login.ProfileActivity;
+import com.ukcovid19project.Models.Jsons;
+import com.ukcovid19project.Models.Users;
+import com.ukcovid19project.OrphanageSupport.FreeFoodActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mUsersDatabase;
+    private RecyclerView Grid_List;
+    private FirebaseAuth mAuth;
+    private ImageView Profile, Menu;
+    private FloatingActionButton Alarm;
+
+    String[] web = {
+            "Corona Status",
+            "Home Treatment",
+            "My Health Status",
+            "Health Cares",
+            "Medical stores",
+            "Doctor Online Appointment",
+            "Hospital Admissions",
+            "Volunteers & Donors",
+            "Food Supply",
+            "Labs for Test",
+            "Support Orphans & Vulnerable",
+            "E-Pass",
+            "Donate Funds",
+            "Online Education",
+            "Government Orders",
+            "Tweets",
+            "FAQs"
+
+    } ;
+    int[] imageId = {
+            R.drawable.corona,
+            R.drawable.home,
+            R.drawable.health,
+            R.drawable.healthcare,
+            R.drawable.medical_store,
+            R.drawable.doctor,
+            R.drawable.hospital,
+            R.drawable.volunteer,
+            R.drawable.food,
+            R.drawable.lab,
+            R.drawable.support,
+            R.drawable.pass,
+            R.drawable.donate,
+            R.drawable.education,
+            R.drawable.governmentorder,
+            R.drawable.tweet,
+            R.drawable.faq
+
+    };
+
+    private TextView Confirmed, Deceased, Tested, Recovered;
+    private RequestQueue mRequestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        Grid_List = findViewById(R.id.grid_view);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),3, LinearLayoutManager.VERTICAL,false);
+        Grid_List.setLayoutManager(gridLayoutManager);
+
+        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        Confirmed = findViewById(R.id.confirmed_count);
+        Recovered = findViewById(R.id.cured_count);
+        Tested = findViewById(R.id.tested_count);
+        Deceased  = findViewById(R.id.deceased_count);
+
+        MainAdapter mainAdapter = new MainAdapter(MainActivity.this, web, imageId);
+        Grid_List.setAdapter(mainAdapter);
+
+        parseJSON();
+
+        Profile = findViewById(R.id.toolbar_profile);
+        Menu = findViewById(R.id.toolbar_menu);
+        Profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra("access","true");
+                startActivity(intent);
+            }
+        });
+
+
+
+        Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, v);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.logout:
+                                FirebaseAuth.getInstance().signOut();
+                                sendToLogin();
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popupMenu.inflate(R.menu.menu_main);
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser mCurrentUser = mAuth.getCurrentUser();
+
+        if (mCurrentUser == null) {
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        else{
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            mUsersDatabase = FirebaseDatabase.getInstance().getReference("Users").child(auth.getCurrentUser().getUid());
+            mUsersDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Users user = dataSnapshot.getValue(Users.class);
+                    if (user.getVerified().equals("false")) {
+                        Intent setupIntent = new Intent(MainActivity.this, ProfileActivity.class);
+                        setupIntent.putExtra("access","false");
+                        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(setupIntent);
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).create().show();
+    }
+
+    private void parseJSON() {
+        JsonObjectRequest request = new JsonObjectRequest("https://api.covid19india.org/v3/data.json", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject("AN").getJSONObject("total");
+                            String confirmed = jsonObject.getString("confirmed");
+                            String tested = jsonObject.getString("tested");
+                            String recovered = jsonObject.getString("recovered");
+
+                            Confirmed.setText(confirmed);
+                            Deceased.setText("0");
+                            Tested.setText(tested);
+                            Recovered.setText(recovered);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
+    }
+}
